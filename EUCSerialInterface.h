869204dde42543ee38a9aff44e0fcebceee375e8
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-10 16:51:07 +0300

    Add new files

---
+++ EUCSerialInterface.h
@@ -0,0 +1,11 @@
+#ifndef EUC_SERIAL_INTERFACE_H
+#define EUC_SERIAL_INTERFACE_H
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+ #include <pins_arduino.h>
+#endif
+
+#endif // EUC_SERIAL_INTERFACE_H
