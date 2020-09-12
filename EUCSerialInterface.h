8d3c3051a8d00a72c53bd11619d28ab3da5c8573
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-13 01:13:46 +0300

    update header file

--- EUCSerialInterface.h
+++ EUCSerialInterface.h
@@ -10,6 +10,35 @@
 
 class EUC {
     public:
+        struct RawData {
+            unsigned char headerPrimaryPacket[8] = {0x04, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
+            unsigned char voltage[2] = {0x00, 0x00};
+            unsigned char speed[2] = {0x00, 0x00};
+            unsigned char tempMileage[4] = {0x00, 0x00, 0x00, 0x00};
+            unsigned char current[2] = {0x00, 0x00};
+            unsigned char temperature[2] = {0x00, 0x00};
+            //unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
+            unsigned char unknownData[4] = {0x00, 0x00, 0x00, 0x00}; // TODO: figure out what that is //NOTE: only first byte seems to change
+            unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
+            unsigned char mileage[4] = {0x00, 0x00, 0x00, 0x00};
+            unsigned char end[12] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+            
+            bool dataIsNew = false;
+        };
+
+        struct UsableData {
+            float voltage;
+            float speed;
+            float tempMileage;
+            float current;
+            float temperature;
+            //unsigned char unknownData[4];
+            float mileage;
+            bool dataIsNew = false;
+        };
+        
+    
+        Euc(Stream &ReceiverSerial, Stream &TransmitterSerial);
         void checkData();
         void setCallback(void (*eucLoopCallback)(float, float, float, float, float, float, bool));
 
@@ -18,7 +47,8 @@ class EUC {
         void setAlarms(int level, bool state);
         void calibrateAlignment();
         void set6kmhTiltback(bool state);
-        
+    private:
+
 }
 
 #endif // EUC_SERIAL_INTERFACE_H
