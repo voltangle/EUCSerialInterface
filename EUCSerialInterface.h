b11421edfd6f54e564d72af77bffecd3535b711b
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-13 11:38:20 +0300

    update header

--- EUCSerialInterface.h
+++ EUCSerialInterface.h
@@ -1,12 +1,7 @@
 #ifndef EUC_SERIAL_INTERFACE_H
 #define EUC_SERIAL_INTERFACE_H
 
-#if (ARDUINO >= 100)
- #include <Arduino.h>
-#else
- #include <WProgram.h>
- #include <pins_arduino.h>
-#endif
+#include <Arduino.h>
 
 class EUC {
     public:
@@ -15,36 +10,6 @@ class EUC {
         void (*eucLoop)(float,float,float,float,float,float,bool);
     
         EUC(Stream &ReceiverSerial, Stream &TransmitterSerial);
-    private:
-        class GotWayDecoder {
-            struct RawData {
-                unsigned char headerPrimaryPacket[8] = {0x04, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
-                unsigned char voltage[2] = {0x00, 0x00};
-                unsigned char speed[2] = {0x00, 0x00};
-                unsigned char tempMileage[4] = {0x00, 0x00, 0x00, 0x00};
-                unsigned char current[2] = {0x00, 0x00};
-                unsigned char temperature[2] = {0x00, 0x00};
-                //unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
-                unsigned char unknownData[4] = {0x00, 0x00, 0x00, 0x00}; // TODO: figure out what that is //NOTE: only first byte seems to change
-                unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
-                unsigned char mileage[4] = {0x00, 0x00, 0x00, 0x00};
-                unsigned char end[12] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-                
-                bool dataIsNew = false;
-            };
-            struct UsableData {
-                float voltage;
-                float speed;
-                float tempMileage;
-                float current;
-                float temperature;
-                //unsigned char unknownData[4];
-                float mileage;
-                bool dataIsNew = false;
-            };
-        }
-        Euc::RawData receiveRawData();
-        EUC::UsableData makeRawDataUsable(EUC::RawData eucRawData);
 };
 
 class GWMSX: public EUC {
@@ -57,6 +22,34 @@ class GWMSX: public EUC {
         void setAlarms(int level, bool state);
         void calibrateAlignment();
         void set6kmhTiltback(bool state);
+
+        struct RawData {
+            unsigned char headerPrimaryPacket[8] = {0x04, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
+            unsigned char voltage[2] = {0x00, 0x00};
+            unsigned char speed[2] = {0x00, 0x00};
+            unsigned char tempMileage[4] = {0x00, 0x00, 0x00, 0x00};
+            unsigned char current[2] = {0x00, 0x00};
+            unsigned char temperature[2] = {0x00, 0x00};
+            //unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
+            unsigned char unknownData[4] = {0x00, 0x00, 0x00, 0x00}; // TODO: figure out what that is //NOTE: only first byte seems to change
+            unsigned char headerSecondaryPacket[8] = {0x00, 0x18, 0x5A, 0x5A, 0x5A, 0x5A, 0x55, 0xAA};
+            unsigned char mileage[4] = {0x00, 0x00, 0x00, 0x00};
+            unsigned char end[12] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+            
+            bool dataIsNew = false;
+        };
+        struct UsableData {
+            float voltage;
+            float speed;
+            float tempMileage;
+            float current;
+            float temperature;
+            //unsigned char unknownData[4];
+            float mileage;
+            bool dataIsNew = false;
+        };
+        Euc::RawData receiveRawData();
+        EUC::UsableData makeRawDataUsable(EUC::RawData eucRawData);
 };
 
 #endif // EUC_SERIAL_INTERFACE_H
