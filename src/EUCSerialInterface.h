21b0d1284cb2998b06c15da21bceb3a7112f2829
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-28 10:33:13 +0300

    removed support for old arduino IDE versions

--- src/EUCSerialInterface.h
+++ src/EUCSerialInterface.h
@@ -1,12 +1,7 @@
 #ifndef EUC_SERIAL_INTERFACE_H
 #define EUC_SERIAL_INTERFACE_H
 
-#if (ARDUINO >= 100)
- #include <Arduino.h>
-#else
- #include <WProgram.h>
- #include <pins_arduino.h>
-#endif
+#include <Arduino.h>
 
 class GotwayMcm2 {
     public:
